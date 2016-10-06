dec72557ff4f8a7fe938acd79b791881558c50bc
Ben Griffiths <mrbengriffiths@googlemail.com>
2016-10-06 21:16:26 +0100

    Created by Atomist project editor atomist-project-templates.spring-boot-common-editors.AddHystrix

--- src/main/java/com/myorg/MyServiceApplication.java
+++ src/main/java/com/myorg/MyServiceApplication.java
@@ -2,8 +2,10 @@ package com.myorg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class MyServiceApplication {
 
     public static void main(String[] args) {
